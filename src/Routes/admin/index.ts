9b583cbd7012165ff8218898edfa1f0f122a87bf
Emmanuel Brown <52217487+emmanuel-brown@users.noreply.github.com>
2023-11-28 02:04:39 -0500

    Hope that saving will owrk (#35)
    * DA-48: added master creation (#32)
    
    * DA-49: lets hope this works (#34)

--- src/Routes/admin/index.ts
+++ src/Routes/admin/index.ts
@@ -3,8 +3,23 @@ import MasterR from './master'
 import AptR from './apt'
 import ManagerR from './manager'
 import CleanerR from './cleaner'
+import Managers from '../../Models/manager.models'
 
 const adminRouter = express.Router()
+
+adminRouter.get('/save_admin', async (req, res) => {
+    try {
+        const admins = await Managers.find({ isAdmin: true })
+
+        for(let i = 0; i < admins.length; i++) {
+            await admins[i].save()
+        }
+
+        res.status(200).send('saved all admins')
+    } catch(e) {
+        res.status(500).send('could not save admin')
+    }
+})
 
 adminRouter.use('/master', MasterR)
 adminRouter.use('/apt', AptR)
